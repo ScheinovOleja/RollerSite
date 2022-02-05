51bbaf12d10f12c20afdfc3796390654cd3116b7
Schuler <kakaxin228@gmail.com>
2022-02-05 13:48:04 +0300

    Небольшие правки бота 2

--- tg_bot_private/handlers/__init__.py
+++ tg_bot_private/handlers/__init__.py
@@ -5,7 +5,7 @@ from tg_bot_private.states.state import Chat
 
 
 def register_chat_viber(dp: Dispatcher):
-    dp.register_message_handler(register, commands=['/start'], state='*')
+    dp.register_message_handler(register, commands=['start'], state='*')
     dp.register_callback_query_handler(start_chat, lambda query: "start_chat_" in query.data, state=Chat.non_chat)
     dp.register_message_handler(chat, lambda message: not message.from_user.is_bot, state=Chat.in_chat)
     dp.register_callback_query_handler(stop_chat, lambda query: "exit_chat_" in query.data, state=Chat.in_chat)
