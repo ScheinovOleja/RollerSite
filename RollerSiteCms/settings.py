1c5fedfb18b16b955c02adb54656a16502dc63d7
Schuler <kakaxin228@gmail.com>
2022-02-08 16:41:00 +0300

    Добавил кастомные шрифты в ckeditor

--- RollerSiteCms/settings.py
+++ RollerSiteCms/settings.py
@@ -228,6 +228,16 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
         'toolbarCanCollapse': False,
         'forcePasteAsPlainText': False,
+        'font_names': 'Arial/Arial, Helvetica, sans-serif;' +
+                      'Comic Sans MS/Comic Sans MS, cursive;' +
+                      'Courier New/Courier New, Courier, monospace;' +
+                      'Georgia/Georgia, serif;' +
+                      'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+                      'Tahoma/Tahoma, Geneva, sans-serif;' +
+                      'Times New Roman/Times New Roman, Times, serif;' +
+                      'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+                      'Verdana/Verdana, Geneva, sans-serif;' +
+                      "Montserrat, sans-serif;"
     },
 }
 
