6325459abc3ed68fc640121afe422d20d7dc3b69
Schuler <kakaxin228@gmail.com>
2022-02-08 17:41:45 +0300

    Убрал staticfile_finder. Мешал работе плагину link/button

--- RollerSiteCms/settings.py
+++ RollerSiteCms/settings.py
@@ -194,10 +194,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [STATIC_DIR]
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
 
 LOGIN_REDIRECT_URL = '/ru/personal_area/'
 
